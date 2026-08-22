02e0293109eb3837de34533b8fd2a66e74a5c75d
fallw1nd <60083508+fallw1nd@users.noreply.github.com>
2026-08-22 19:57:47 +0800

    test(ui): stabilize GSAP animation frame environment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,14 @@ import { edition } from "./data/brief";
 import type { BriefManifest, BriefSearchIndex } from "./types";
 
 vi.hoisted(() => {
+  Object.defineProperty(globalThis, "requestAnimationFrame", {
+    configurable: true,
+    value: (callback: FrameRequestCallback) => window.setTimeout(() => callback(performance.now()), 0),
+  });
+  Object.defineProperty(globalThis, "cancelAnimationFrame", {
+    configurable: true,
+    value: (handle: number) => window.clearTimeout(handle),
+  });
   Object.defineProperty(window, "matchMedia", {
     configurable: true,
     value: (query: string) => ({
